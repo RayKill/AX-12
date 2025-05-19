ce699b8dcd935377d3cae7f6764e1fa1c26b9b89
RayKill <rayan.djou@gmail.com>
2025-05-19 11:47:32 +0200

    V0.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,11 @@
 #include "lvgl.h"
-#include "app_hal.h"
-#include "ax12.h"
-#include "usart.h"
-
-#include <cstdio>
-
-static uint8_t servoID = 1;
 
 static void event_handler(lv_event_t * e)
 {
     lv_event_code_t code = lv_event_get_code(e);
 
     if(code == LV_EVENT_CLICKED) {
-        LV_LOG_USER("Clicked - Servo action");
-
-        ax12_ping(servoID);
-
-        uint16_t pos = ax12_read_position(servoID);
-        if(pos != 0xFFFF) {
-            LV_LOG_USER("Current position: %d", pos);
-            uint16_t newPos = pos + 50;
-            if(newPos > 1023) newPos = 1023;
-            ax12_move_to_position(servoID, newPos);
-        } else {
-            LV_LOG_USER("Servo read failed");
-        }
+        LV_LOG_USER("Clicked");
     }
     else if(code == LV_EVENT_VALUE_CHANGED) {
         LV_LOG_USER("Toggled");
@@ -33,43 +14,84 @@ static void event_handler(lv_event_t * e)
 
 void testLvgl()
 {
-    lv_obj_t * label;
+  // Initialisations générales
+  lv_obj_t * label;
 
-    lv_obj_t * btn1 = lv_button_create(lv_screen_active());
-    lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
-    lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
+  lv_obj_t * btn1 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn1, event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn1, LV_ALIGN_CENTER, 0, -40);
+  lv_obj_remove_flag(btn1, LV_OBJ_FLAG_PRESS_LOCK);
 
-    label = lv_label_create(btn1);
-    lv_label_set_text(label, "Move Servo");
-    lv_obj_center(label);
+  label = lv_label_create(btn1);
+  lv_label_set_text(label, "Button");
+  lv_obj_center(label);
 
-    lv_obj_t * btn2 = lv_button_create(lv_screen_active());
-    lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
-    lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
-    lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
-    lv_obj_set_height(btn2, LV_SIZE_CONTENT);
+  lv_obj_t * btn2 = lv_button_create(lv_screen_active());
+  lv_obj_add_event_cb(btn2, event_handler, LV_EVENT_ALL, NULL);
+  lv_obj_align(btn2, LV_ALIGN_CENTER, 0, 40);
+  lv_obj_add_flag(btn2, LV_OBJ_FLAG_CHECKABLE);
+  lv_obj_set_height(btn2, LV_SIZE_CONTENT);
 
-    label = lv_label_create(btn2);
-    lv_label_set_text(label, "Toggle");
-    lv_obj_center(label);
+  label = lv_label_create(btn2);
+  lv_label_set_text(label, "Toggle");
+  lv_obj_center(label);
 }
+
+#ifdef ARDUINO
+
+#include "lvglDrivers.h"
+
+// à décommenter pour tester la démo
+// #include "demos/lv_demos.h"
+
+void mySetup()
+{
+  // à décommenter pour tester la démo
+  // lv_demo_widgets();
+
+  // Initialisations générales
+  testLvgl();
+}
+
+void loop()
+{
+  // Inactif (pour mise en veille du processeur)
+}
+
+void myTask(void *pvParameters)
+{
+  // Init
+  TickType_t xLastWakeTime;
+  // Lecture du nombre de ticks quand la tâche débute
+  xLastWakeTime = xTaskGetTickCount();
+  while (1)
+  {
+    // Loop
+
+    // Endort la tâche pendant le temps restant par rapport au réveil,
+    // ici 200ms, donc la tâche s'effectue toutes les 200ms
+    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(200)); // toutes les 200 ms
+  }
+}
+
+#else
+
+#include "lvgl.h"
+#include "app_hal.h"
+#include <cstdio>
 
 int main(void)
 {
-    printf("LVGL + AX-12 Demo\n");
-    fflush(stdout);
+  printf("LVGL Simulator\n");
+  fflush(stdout);
 
-    HAL_Init();
-    SystemClock_Config();       // Auto-généré par CubeMX
-    MX_GPIO_Init();
-    MX_USART1_UART_Init();     // UART1 @ 1 Mbps
+  lv_init();
+  hal_setup();
 
-    lv_init();
-    hal_setup();
+  testLvgl();
 
-    testLvgl();
-
-    hal_loop();
-    return 0;
+  hal_loop();
+  return 0;
 }
+
+#endif
